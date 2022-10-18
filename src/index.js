574b226d663ad9fa81a76bf9e5769af2a6207b60
walissonsou <walissonsouza96@hotmail.com>
2022-10-18 15:48:19 -0300

    Should be able to update a todo

--- src/index.js
+++ src/index.js
@@ -69,7 +69,18 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline} = request.body;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return res.status(200).json(todo);
+
+
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
